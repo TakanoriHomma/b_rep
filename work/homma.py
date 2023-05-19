8648164175bbceeb17a80cc7334951f0fb4e2df1
TakanoriHomma <takasama_1205@yahoo.co.jp>
2023-05-19 19:13:06 +0900

    adding some changes to get confliction

--- work/homma.py
+++ work/homma.py
@@ -1,6 +1,8 @@
-print('fukuchan is hentai')
+print('fukuchan is hentai.')
 
-print('Mr.gohn likes ramen')
+print("fukuchan is from Hiroshima.")
+
+print('Mr.gohn likes ramen.')
 print("I'm Takanori Homma.")
 
 print("This is second commit by homma.")
